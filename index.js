9368c18a2e9bfc58df843b578e2f0ea6e2b7ab61
Cola <cscolabear@gmail.com>
2019-05-09 17:49:41 +0800

    調整為多縣市

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const Browser = require('./src/Browser');
 const fs = require('fs');
 
-(async () => {
-  let result;
+let result = [];
+const countys = {
+  63: 'Taipei City',
+  65: 'New Taipei City',
+  66: 'Taichung City',
+  67: 'Tainan City',
+  64: 'Kaohsiung City',
+};
 
+const getWeather = async (target_url) => {
+  let result;
   try {
-    const page = await Browser.open('https://www.cwb.gov.tw/V8/C/W/County/County.html?CID=63');
+    const page = await Browser.open(target_url);
     await page.setViewport({ width: 1280, height: 800, });
 
-    result = await page.evaluate(() => {
+    result = await page.evaluate((target_url) => {
 
       const getTextContent = (selector_string, default_string = '') => {
         return document.querySelector(selector_string)
@@ -19,24 +27,38 @@ const fs = require('fs');
       const county = getTextContent('.main-title', 'no county');
       const current_title = getTextContent('.to-to li .title');
       const description = getTextContent('#marquee_1', 'no description');
+      const temperature = getTextContent('.to-to li .tem-C, is-active') + '˚C';
       const rain = getTextContent('.to-to li .rain');
       const updated_at = Date.now();
 
-      return { county, current_title, description, rain, updated_at };
-    });
+      return { county, current_title, description, temperature, rain, target_url, updated_at };
+    }, target_url);
 
   } catch (err) {
     console.error(err);
+
   } finally {
-    await Browser.exit();
-
-    console.table(result);
-    fs.writeFile("./data/json", JSON.stringify(result), function (err) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("The file was saved!");
-    });
+    return result;
   }
+};
 
-})();
+Object.keys(countys).map(cid => {
+  target_url = `https://www.cwb.gov.tw/V8/C/W/County/County.html?CID=${cid}`;
+  console.log(`${countys[cid]}: ${target_url}`);
+  result.push(getWeather(target_url))
+});
+
+Promise.all(result).then(values => {
+  Browser.exit();
+  console.table(values);
+  fs.writeFileSync("./data/json", JSON.stringify(values), function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
+  console.log("The file was saved!");
+  process.exit();
+}).catch(reason => {
+  console.error(reason);
+});
+
