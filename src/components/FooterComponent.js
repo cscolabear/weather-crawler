70953eec493abb7512c6b01e0d9d028e6cd2c124
Cola <cscolabear@gmail.com>
2019-06-14 17:04:27 +0800

    update build command

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -11,14 +11,14 @@ function FooterComponent() {
       <p>Coding by Cola</p>
 
       <div className="github-area">
-        <div className="nes-balloon is-rounded-dark from-left ">
+        {/* <div className="nes-balloon is-rounded-dark from-left ">
           <p>
               used <i className="nes-jp-logo"></i> NES.css + <i className="polar-bear"></i> Cola
           </p>
           <p>
             <i className="nes-icon star"></i> Please~~~
           </p>
-        </div>
+        </div> */}
         <a href="https://github.com/cscolabear/weather-crawler" target="_blank" rel="noopener noreferrer">
           <i className="nes-octocat animate"></i>
         </a>
