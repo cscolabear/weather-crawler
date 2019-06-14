8cfd54c069e695c0eb218fed349a9a647f9d36d3
Cola <cscolabear@gmail.com>
2019-06-14 17:46:20 +0800

    remove docs gitignore

---
+++ docs/precache-manifest.67059b37511cfae528bc6e4aad68be5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed8d4f2144cc1ca8d447b268fb43d33",
+    "url": "/weather-crawler/build/index.html"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/build/static/css/2.b18ee16a.chunk.css"
+  },
+  {
+    "revision": "16b68c0ac579ecd079d4",
+    "url": "/weather-crawler/build/static/css/main.fd6aa244.chunk.css"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/build/static/js/2.f3d6c536.chunk.js"
+  },
+  {
+    "revision": "16b68c0ac579ecd079d4",
+    "url": "/weather-crawler/build/static/js/main.314e96f7.chunk.js"
+  },
+  {
+    "revision": "0ff47a3c5e2e5d180703",
+    "url": "/weather-crawler/build/static/js/runtime~main.212a264b.js"
+  }
+]);
---
+++ docs/precache-manifest.c797751bf3714a7441d948ac12fe61a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ed0c21de34a174f374875309e58915",
+    "url": "/weather-crawler/build/index.html"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/build/static/css/2.b18ee16a.chunk.css"
+  },
+  {
+    "revision": "69115cc52cdffef3c674",
+    "url": "/weather-crawler/build/static/css/main.b6d5d9fd.chunk.css"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/build/static/js/2.f3d6c536.chunk.js"
+  },
+  {
+    "revision": "69115cc52cdffef3c674",
+    "url": "/weather-crawler/build/static/js/main.314e96f7.chunk.js"
+  },
+  {
+    "revision": "0ff47a3c5e2e5d180703",
+    "url": "/weather-crawler/build/static/js/runtime~main.212a264b.js"
+  }
+]);
