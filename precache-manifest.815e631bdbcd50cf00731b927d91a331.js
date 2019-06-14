e4e0d92fe1151cd911d83e40e418aaae19459c17
Cola <cscolabear@gmail.com>
2019-06-14 10:30:56 +0000

    Updates

---
+++ precache-manifest.815e631bdbcd50cf00731b927d91a331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d074970931eb2196656bfefa9ccb408",
+    "url": "/weather-crawler/index.html"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/static/css/2.b18ee16a.chunk.css"
+  },
+  {
+    "revision": "517fb844d8e408eca9b7",
+    "url": "/weather-crawler/static/css/main.b6d5d9fd.chunk.css"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/static/js/2.f3d6c536.chunk.js"
+  },
+  {
+    "revision": "517fb844d8e408eca9b7",
+    "url": "/weather-crawler/static/js/main.e338b9a3.chunk.js"
+  },
+  {
+    "revision": "2cf69133631eaa0591e8",
+    "url": "/weather-crawler/static/js/runtime~main.fcba40b4.js"
+  }
+]);
