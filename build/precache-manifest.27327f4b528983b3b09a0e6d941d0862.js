4459d5a258168b9e8f21bc02a57d516d4cb329f0
Cola <cscolabear@gmail.com>
2019-06-14 17:51:07 +0800

    remove build ignore rebuild

---
+++ build/precache-manifest.27327f4b528983b3b09a0e6d941d0862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7c623a76ca03bb9b7dc4c3631dcf14",
+    "url": "/weather-crawler/build/index.html"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/build/static/css/2.b18ee16a.chunk.css"
+  },
+  {
+    "revision": "517fb844d8e408eca9b7",
+    "url": "/weather-crawler/build/static/css/main.b6d5d9fd.chunk.css"
+  },
+  {
+    "revision": "3e56495ad59283ebd89f",
+    "url": "/weather-crawler/build/static/js/2.f3d6c536.chunk.js"
+  },
+  {
+    "revision": "517fb844d8e408eca9b7",
+    "url": "/weather-crawler/build/static/js/main.e338b9a3.chunk.js"
+  },
+  {
+    "revision": "0ff47a3c5e2e5d180703",
+    "url": "/weather-crawler/build/static/js/runtime~main.212a264b.js"
+  }
+]);
