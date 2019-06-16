44d2ebd1b5c16ab9f6354ecc7ddc217a0fcaecda
Cola <cscolabear@gmail.com>
2019-06-16 07:06:46 +0000

    Updates

--- precache-manifest.db1cc16ce0642172df87cac156277f16.js
+++ precache-manifest.db1cc16ce0642172df87cac156277f16.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23540cf0e5ba24e95fe406f40f005075",
+    "revision": "8bb364384e1d4d4ba9e7e1a90de5c460",
     "url": "/weather-crawler/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weather-crawler/static/css/2.b18ee16a.chunk.css"
   },
   {
-    "revision": "bf0504a7fb61d68ec525",
-    "url": "/weather-crawler/static/css/main.f3643eda.chunk.css"
+    "revision": "acb0b5ff82cd9abc5972",
+    "url": "/weather-crawler/static/css/main.d0b86e7a.chunk.css"
   },
   {
     "revision": "3e56495ad59283ebd89f",
     "url": "/weather-crawler/static/js/2.f3d6c536.chunk.js"
   },
   {
-    "revision": "bf0504a7fb61d68ec525",
-    "url": "/weather-crawler/static/js/main.e338b9a3.chunk.js"
+    "revision": "acb0b5ff82cd9abc5972",
+    "url": "/weather-crawler/static/js/main.a2135b22.chunk.js"
   },
   {
     "revision": "2cf69133631eaa0591e8",
