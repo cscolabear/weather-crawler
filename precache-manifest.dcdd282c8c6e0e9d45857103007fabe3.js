1365808be95c00890c51debb7b320eeef9b96118
travis-ci <cscolabear+travis@gmail.com>
2019-08-21 02:59:26 +0000

    Updates

--- precache-manifest.dcdd282c8c6e0e9d45857103007fabe3.js
+++ precache-manifest.dcdd282c8c6e0e9d45857103007fabe3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0f5c5199b876d7d7c6ed692bed4f875b",
+    "revision": "420ca4c8f5c4e40ca302f758b0abf645",
     "url": "/weather-crawler/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weather-crawler/static/css/2.b18ee16a.chunk.css"
   },
   {
-    "revision": "c2f26eeaa7097d8d5dec",
+    "revision": "98ae1fa6637261acb8cc",
     "url": "/weather-crawler/static/css/main.a057fd2b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weather-crawler/static/js/2.3f06d9f0.chunk.js"
   },
   {
-    "revision": "c2f26eeaa7097d8d5dec",
-    "url": "/weather-crawler/static/js/main.1cf86c69.chunk.js"
+    "revision": "98ae1fa6637261acb8cc",
+    "url": "/weather-crawler/static/js/main.511db38e.chunk.js"
   },
   {
     "revision": "2cf69133631eaa0591e8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-crawler/precache-manifest.45111562da93c7076c0be4aa42075e68.js"
+  "/weather-crawler/precache-manifest.dcdd282c8c6e0e9d45857103007fabe3.js"
 );
 
 self.addEventListener('message', (event) => {
