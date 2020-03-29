37ee173d6b5ed358918b495153767b8b7c878d36
travis-ci <cscolabear+travis@gmail.com>
2020-03-29 02:53:22 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-crawler/precache-manifest.6eb563df92cd6d0eb228f78bf3925453.js"
+  "/weather-crawler/precache-manifest.62b983fd2f6cfc6d78d89e6b5f60bda9.js"
 );
 
 self.addEventListener('message', (event) => {
