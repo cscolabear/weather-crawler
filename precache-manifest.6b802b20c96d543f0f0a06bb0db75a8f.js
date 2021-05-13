721b75b488f78a848fb600efe4f46183615a9a4e
travis-ci <cscolabear+travis@gmail.com>
2021-05-13 16:58:09 +0000

    Updates

--- precache-manifest.6b802b20c96d543f0f0a06bb0db75a8f.js
+++ precache-manifest.6b802b20c96d543f0f0a06bb0db75a8f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8933b7eb9041cb9b36564c0cf3904ad8",
+    "revision": "fc8cde71ea553b370967f17bf72630c6",
     "url": "/weather-crawler/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weather-crawler/static/css/2.b18ee16a.chunk.css"
   },
   {
-    "revision": "8bb0921e765d235ac6fa",
+    "revision": "79a04d3be99d2975e491",
     "url": "/weather-crawler/static/css/main.a057fd2b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weather-crawler/static/js/2.3f06d9f0.chunk.js"
   },
   {
-    "revision": "8bb0921e765d235ac6fa",
-    "url": "/weather-crawler/static/js/main.bb19e5c0.chunk.js"
+    "revision": "79a04d3be99d2975e491",
+    "url": "/weather-crawler/static/js/main.49ae0cd2.chunk.js"
   },
   {
     "revision": "2cf69133631eaa0591e8",
